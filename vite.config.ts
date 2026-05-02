efdbcf35a294408a63a3aa6d1e4a017986d216ed
Samuel <me@samuelcheston.com>
2026-05-02 12:49:51 +0000

    add a dev relay

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+    server: {
+    proxy: {
+      '/relay.php': {
+        target: 'http://hrpauth.samuelcheston.com', // 你的目标后端
+        changeOrigin: true,
+
+        // 🔥 关键：把 /relay.php 去掉，后面的路径原样透传
+        rewrite: (path) => path.replace(/^\/relay\.php/, '')
+      }
+    }
+  }
 })
